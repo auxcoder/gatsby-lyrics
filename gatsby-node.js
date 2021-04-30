1bf02e42daaf132406ff075bd265648c6d8c57a2
Pavel Duran <auxcoder@gmail.com>
2021-04-29 23:17:16 -0400

    Add page to render a single lyrics

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,37 @@
-// dynamic pages
+const path = require('path')
+
+exports.createPages = ({boundActionCreators, graphql}) => {
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              track
+              path
+              title
+              date
+              author
+              album
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) return Promise.reject(res.errors)
+
+    const {createPage} = boundActionCreators
+    const entries = res.data.allMarkdownRemark.edges
+
+    // create page for each lyric
+    entries.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: path.resolve('src/templates/lyric-page.js'),
+      })
+    })
+  })
+}
