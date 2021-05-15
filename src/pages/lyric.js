1a201266fecce550ecbbf7851bccd866b6945191
Pavel Duran <auxcoder@gmail.com>
2021-05-14 22:07:37 -0400

    Add styles and covers for tracks and albums, group tracks by album

--- src/pages/lyric.js
+++ src/pages/lyric.js
@@ -1,22 +1,32 @@
-import "./lyric.css"
 import React from 'react'
 import Link from 'gatsby-link'
 import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image"
 // components
 import Layout from "../layouts/index";
+// styles
+import "./lyric.scss"
 //
 const lyricTemplate = ({data}) => (
   <Layout>
-    <Link to={`/album/${data.markdownRemark.fields.album_slug}`}>Go to Album {data.markdownRemark.frontmatter.album}</Link>
-    <hr />
-    <h1>{data.markdownRemark.frontmatter.title}</h1>
-    <div className="meta">
-      <div><small>Track: {data.markdownRemark.frontmatter.track} </small></div>
-      <div><small>Album: {data.markdownRemark.frontmatter.album} </small></div>
-      <div><small>Author: {data.markdownRemark.frontmatter.author} </small></div>
-      <div><small>Released on: {data.markdownRemark.frontmatter.date} </small></div>
+    <div id="track">
+      <Link to={`/album/${data.markdownRemark.fields.album_slug}`}>Go to Album {data.markdownRemark.frontmatter.album}</Link>
+
+      <hr />
+
+      <div className="track-header">
+        <GatsbyImage className="album-cover" image={data.markdownRemark.frontmatter.cover?.childImageSharp.gatsbyImageData} alt="Album cover" />
+        <h1 className="track-title">{data.markdownRemark.frontmatter.title}</h1>
+      </div>
+
+      <div className="meta">
+        <div><small>Track: {data.markdownRemark.frontmatter.track} </small></div>
+        <div><small>Album: {data.markdownRemark.frontmatter.album} </small></div>
+        <div><small>Author: {data.markdownRemark.frontmatter.author} </small></div>
+        <div><small>Released on: {data.markdownRemark.frontmatter.date} </small></div>
+      </div>
+      <div className="lyric" dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
     </div>
-    <div className="lyric" dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
   </Layout>
 )
 
@@ -31,6 +41,15 @@ query LyricItem($album: String, $title: String) {
       date
       album
       track
+      cover {
+        childImageSharp {
+          gatsbyImageData(
+            width: 50
+            placeholder: BLURRED
+            formats: [AUTO, WEBP]
+          )
+        }
+      }
     }
     fields {
       album_slug
--- src/pages/lyrics.js
+++ src/pages/lyrics.js
@@ -1,37 +1,69 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import { graphql } from 'gatsby'
+import { GatsbyImage } from "gatsby-plugin-image"
 // components
 import Layout from "../layouts/index";
+// styles
+import "./lyrics.scss"
 //
 const LyricsPage = ({data}) => (
   <Layout>
-    <h1>All Lyrics</h1>
+    <div id="allAlbums">
+      <h1>All Lyrics</h1>
 
-    {data.allMarkdownRemark.edges.map(item => (
-      <div key={item.node.id}>
-        <Link to={`slug${item.node.frontmatter.title_slug}`}>{item.node.frontmatter.track} - {item.node.frontmatter.title}</Link>
-      </div>
-    ))}
+      {data.allMarkdownRemark.group.map((grp, aidx) => (
+        <div className="album" key={aidx}>
+          {grp.edges.map(item => (
+            <div key={item.node.id}>
+              <Link to={`../${item.node.frontmatter.title_slug}`} className="track">
+                <div className="track-number">{item.node.frontmatter.track}</div>
+
+                <GatsbyImage className="track-cover" image={item.node.frontmatter.cover?.childImageSharp.gatsbyImageData} alt="Album cover" />
+
+                <div className="track-meta">
+                  <div className="track-title">{item.node.frontmatter.title}</div>
+                  <div className="track-author"><small>Author: {item.node.frontmatter.author}</small></div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      ))
+      }
+    </div>
   </Layout>
 )
 
 export const pageQuery = graphql`
 query LyricIndexQuery {
   allMarkdownRemark(sort: {fields: [frontmatter___album, frontmatter___track], order: ASC}) {
-    edges {
-      node {
-        id
-        frontmatter {
-          track
-          title_slug
-          title
-          date
-          author
+    group(field: frontmatter___album) {
+      edges {
+        node {
+          id
+          frontmatter {
+            album
+            track
+            title_slug
+            title
+            date
+            author
+            cover {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 50
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP]
+                )
+              }
+            }
+          }
         }
       }
     }
   }
-}`
+}
+`
 
 export default LyricsPage
